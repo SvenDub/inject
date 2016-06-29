f767c1ff3316eb7ffe7d29a2cda16d0041e9caad
Sven Dubbeld <sven@bitsensor.io>
2016-06-29 12:20:45 +0200

    Add Javadoc to Exceptions

--- src/main/java/nl/svendubbeld/inject/exception/ConstructorException.kt
+++ src/main/java/nl/svendubbeld/inject/exception/ConstructorException.kt
@@ -1,3 +1,6 @@
 package nl.svendubbeld.inject.exception
 
+/**
+ * Thrown when there is no suitable constructor available.
+ */
 class ConstructorException : Exception("Type contains no constructors.")
--- src/main/java/nl/svendubbeld/inject/exception/TypeNotResolvedException.kt
+++ src/main/java/nl/svendubbeld/inject/exception/TypeNotResolvedException.kt
@@ -1,5 +1,11 @@
 package nl.svendubbeld.inject.exception
 
+/**
+ * Thrown when the requested type could not be resolved.
+ */
 class TypeNotResolvedException(type: Class<*>) : Exception("Could not resolve ${type.toString()}.") {
-    var type: Class<*> = type;
+    /**
+     * The type that could not be resolved.
+     */
+    val type: Class<*> = type;
 }
